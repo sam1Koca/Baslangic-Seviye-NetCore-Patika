89c6831d5f0f5c4202bfd1254f6d8d202f1d697b
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-07-19 19:57:25 +0300

    HackerRank Day2 Ödevi Tamamlandı.

---
+++ Assignments/Day2-Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day2_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double tipCost = meal_cost * tip_percent / 100;
+            double taxtCost = meal_cost * tax_percent / 100;
+            double totalCost = meal_cost + tipCost + taxtCost;
+        
+            Console.WriteLine(Math.Round(totalCost)); // Math.Round ile en yakn integer değere yuvarla. 
+        }
+    }
+}
