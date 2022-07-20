e120b4ac97c5ad568990778b5b9889a71b4e9ef0
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-07-20 19:01:32 +0300

    Tip Donusumleri Ders Odevi Tamamlandı..

---
+++ Assignments/Solve-Me-First/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solve_Me_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1,val2);
+            Console.WriteLine(sum);
+        }
+
+        static int solveMeFirst(int a, int b) { 
+
+            return a+b;
+        }
+    }
+}
