81fd5a2e5d6de9b496287f5b4459fea45be54692
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-07-28 14:16:07 +0300

    Day3-HackerRank-Conditional-Statements Ödevi Tamamlandı.

---
+++ Assignments/Day3-Conditional-Statements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day3_Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        
+        if (N % 2 == 1)
+        {
+            Console.WriteLine("Weird");
+        }
+
+        else if (N % 2 == 0 && N > 2 && N < 5 )
+        {
+            Console.WriteLine("Not Weird");
+        }
+
+        else if (N % 2 == 0 && N > 6 && N <= 20)
+        {
+            Console.WriteLine("Weird");
+        }
+
+        else if (N % 2 == 0 && N > 20)
+        {
+            Console.WriteLine("Not Weird");
+        }
+        
+        }
+    }
+}
