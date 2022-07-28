616839a11a56edb59297d2fd9aa053948642468f
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-07-28 14:01:01 +0300

    İf-Else Yapıları Dersi Tamamlandı.

---
+++ İf-else-if/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace İf_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour; // O an ki saati aldım.
+
+            Console.WriteLine(time);
+
+
+            if (time>=6 && time<11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+
+            string sonuc = time <=18 ? "İyi Günler" : "İyi Geceler"; 
+
+            sonuc = time>=6 && time<11 ? "İyi Günler" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            // Yukarıdaki Koşul sağlanırsa soru işaretinden sonra ilk ifade çalışır.
+            // : ifadesi, değilse anlamına gelmekte. Bu ifade ile bir durumu belirtip sonlandırınca farklı bir koşul daha yazabilirim.
+
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
