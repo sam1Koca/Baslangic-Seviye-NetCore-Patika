dc19ce3bce673b6e904b72a52ad09acbf632ef50
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-07-29 15:23:04 +0300

    HackerRank Day5-Loops Ödevi Tamamlandı.

---
+++ Assignments/Day5-Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day5_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+        
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(n + " x " + i + " = " + (n * i));
+            }         
+
+        }
+    }
+}
