2462f0f452a46f8bdec0a1e12b11d349bd0df23f
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-06 18:32:58 +0300

    Day-7 Arrays HackerRank Çözümü Tamamlandı

---
+++ Assignments/Day7-Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day7_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        
+            for (var i = n - 1; i > -1; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
