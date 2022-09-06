69a13e2d42ef6e96148cc883a76cc8d3e2c59236
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-06 18:09:55 +0300

    BirthdayCakeCandles HackerRank Çözümü Tamamlandı.

---
+++ Assignments/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = candles.First();
+            int count = 1;
+        
+            for(int i = 1; i< candles.Count; i++)
+            {
+            
+                if(candles[i] > max)
+                {
+                    max = candles[i];
+                    count = 1;
+                }
+            
+                else if(candles[i] == max)
+                {
+                    count++; 
+                }
+            }
+        
+            return count;
+        }
+    }
+}
