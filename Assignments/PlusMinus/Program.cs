a73ddf24174453988ea2a968ea2cf01922cbfd7a
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-06 16:13:38 +0300

    Diziler Birinci Bölüm HackerRank Ödevi Tamamlandı.

---
+++ Assignments/PlusMinus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PlusMinus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        double pos = 0;
+        double neg = 0;
+        double zero = 0;
+
+        foreach(int num in arr) {
+            if(num > 0) {
+                pos++;
+            }
+            else if(num < 0) {
+                neg++;
+            }
+            else {
+                zero++;
+            }
+        }
+
+        Console.WriteLine(pos / arr.Length);
+        Console.WriteLine(neg / arr.Length);
+        Console.WriteLine(zero / arr.Length);
+        }
+    }
+}
