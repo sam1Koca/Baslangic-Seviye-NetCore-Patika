cf21fda09317dcc26959d17c593ed28b7772b7a3
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-07 12:42:56 +0300

    Migratory Birds HackerRank Sorusu Çözüldü.

---
+++ Assignments/MigratoryBirds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] count = new int[arr.Max()];
+        int max = 0, c = 0;
+        
+        
+        for(int i=0; i<arr.Count; i++)
+        {
+            count[arr[i]-1]++;
+        }
+        
+        
+        for(int i=0; i<count.Length; i++)
+        {
+            if(max<count[i]) {max = count[i]; c = i; }
+        }
+        
+        
+        return c + 1;
+        }
+    }
+}
