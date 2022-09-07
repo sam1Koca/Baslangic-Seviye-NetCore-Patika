8c9f5419ddff7cc371ab352f1dae7bccba3b4fc1
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-07 12:07:10 +0300

    Compare The Triplets HackerRank Sorusu Çözüldü.

---
+++ Assignments/CompareTheTriplets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> result = new List<int>();
+            int alice = 0, bob = 0;
+            for(int i = 0; i < a.Count; i++){
+            
+            if(a[i] > b[i]){
+                alice++;
+            }
+            
+            else if(b[i] > a[i]){
+                bob++;
+                }
+            }
+            result.Add(alice);
+            result.Add(bob);
+            return result;
+        }
+    }
+}
