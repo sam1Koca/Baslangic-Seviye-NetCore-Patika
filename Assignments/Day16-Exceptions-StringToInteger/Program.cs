f182f17b5bb10810fa392e4cf01768842003578f
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-10 19:31:12 +0300

    Day16-Exceptions-StringToInteger HackerRank Ödevi Tamamlandı.

---
+++ Assignments/Day16-Exceptions-StringToInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day16_Exceptions_StringToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+        
+        try
+        {
+            var i = int.Parse(S);
+            Console.Write(i);
+        }
+        catch
+        {
+            Console.Write("Bad String");
+        }
+        }
+    }
+}
