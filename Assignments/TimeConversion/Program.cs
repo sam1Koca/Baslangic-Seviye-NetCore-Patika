c887bdaf0093d4f617ef932dd8c4eec0e0266ffe
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-11 14:31:27 +0300

    Time Conversion HackerRank Sorusu Çözüldü..

---
+++ Assignments/TimeConversion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = Result.timeConversion(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+    class Result
+    {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+        public static string timeConversion(string s)
+        {
+            bool succesfulConversion = DateTime.TryParse(s, out DateTime time);
+        
+            if(succesfulConversion)
+            {
+                return time.ToString("HH:mm:ss");
+            }
+        
+            return "HackerRank gave me bad input";
+        }
+
+    }
+}
