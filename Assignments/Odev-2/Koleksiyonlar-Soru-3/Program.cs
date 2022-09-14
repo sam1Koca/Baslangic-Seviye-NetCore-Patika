d0dfc390055145103629c33aa66599528397a0fd
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-09-14 19:11:16 +0300

    Ödev-2 Koleksiyonlar-Soru-3 Tamamlandı.

---
+++ Assignments/Odev-2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                -> Klavyeden Girilen Cümle içerisindeki sesli harfleri bir dizi içeriinde saklayan ve dizinin elemanlarını sıralayn programı yazınız.
+            */
+
+            Console.WriteLine("Bir Cümle Giriniz: ");
+            string girilenCumle = Console.ReadLine();
+
+            string sesliHarfler = "aeıioöuü";
+
+            ArrayList sesliDizi = new ArrayList();
+
+            if (!string.IsNullOrEmpty(girilenCumle))
+            {
+                string cumle = girilenCumle.ToLower();
+                for (int i = 0; i < girilenCumle.Length; i++)
+                {
+                    if (sesliHarfler.Contains(cumle[i]))
+                    {
+                        sesliDizi.Add(cumle[i]);
+                    }
+                }
+            }
+
+            sesliDizi.Sort();
+
+            Console.Write("Girilen Cümledeki Sesli Harfler: ");
+            foreach (var harf in sesliDizi)
+            {
+                Console.Write(harf + ",");
+            }
+
+        }
+    }
+}
